8e7cc464b0867f275735aa9024e453ef1bb24987
Austin Blandford <ABlandford@student.neumont.edu>
2018-11-16 09:35:22 -0700

    Organization
    Added pug files along with saved express and pug. Can now test in a link of 'localhost:300'. index.js added as well as a public folder which now contains style.css. Views folder holds pug folders.

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var pug = require('pug');
+var path = require('path');
+var config = require('./config.json')
+
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname + "/public")));
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        'title':'Home',
+        "config":config
+    });
+});
+
+app.get('/:name', function(req, res) {
+    res.render('pages', {
+        "title": req.params.name,
+        "config": config
+    });
+});
+
+app.listen(3000);
