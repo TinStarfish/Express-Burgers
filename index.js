58be89f1e8c84e2c87337046aae10543c37db47b
Austin Blandford <ABlandford@student.neumont.edu>
2018-11-17 09:59:24 -0700

    Button/Dropdown
    I created a button that is on the right side of the nav bar, but I can't figure out how to get it working. Feel free to see if either of you can get the dropdown to work, but I couldn't find anything to help.

--- index.js
+++ index.js
@@ -23,4 +23,21 @@ app.get('/:name', function(req, res) {
     });
 });
 
+function openMenu() {
+    document.getElementById('dropdownMenu').classList.toggle('show');
+}
+
+// window.onclick = function(event) {
+//     if(!event.target.matches('.dropButton')) {
+//         var dropdowns = document.getElementsByClassName('.link_item');
+//         var i;
+//         for(i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if(openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
+
 app.listen(3000);
